897e2eb1c19621142564daaf4c3be803d40b40f1
rassenguy <ftrouw@protonmail.com>
2020-10-04 16:33:46 +0700

    complete burger menu animation

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,23 @@
 // main js for the new new world website
 
-
 // menu-mobile-animation
 function animateMenu() {
     
     var links = document.querySelector(".wrapper-links-bottom");
     var navBtm = document.querySelector("nav.mobile-bottom");
+    var burger = document.querySelector(".hamburger");
+    var burgerTop = document.querySelector(".burger-top");
+    var burgerMiddle = document.querySelector(".burger-middle");
+    var burgerBottom = document.querySelector(".burger-bottom");
     
     // toggle class
     links.classList.toggle("links-reveal");
+    burger.classList.toggle("animate-burger-border");
+    burgerTop.classList.toggle("animate-top");
+    burgerMiddle.classList.toggle("animate-middle");
+    burgerBottom.classList.toggle("animate-bottom");
 
     // remove attributes
     navBtm.style.removeProperty("box-shadow");
-
-    // note - remove background color and filter from :after
 
 }
