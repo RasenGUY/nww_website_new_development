099be238f523069fba2cb35ef44233e45834930e
rassenguy <ftrouw@protonmail.com>
2020-12-07 02:19:59 +0700

    make hamburger menu do things

--- static/js/main.js
+++ static/js/main.js
@@ -3,20 +3,22 @@
 // menu-mobile-animation
 function animateMenu() {
     
-    var links = document.querySelector(".wrapper-links-bottom");
-    var linksUl = document.querySelector(".nav-links-bottom");
-    var navBtm = document.querySelector("nav.mobile-bottom");
-    var burger = document.querySelector(".hamburger");
+    // var links = document.querySelector(".wrapper-links-bottom");
+    // var linksUl = document.querySelector(".nav-links-bottom");
+    // var navBtm = document.querySelector("nav.mobile-bottom");
+    var burger = document.querySelector(".hamburger .burger");
     var burgerTop = document.querySelector(".burger-top");
     var burgerMiddle = document.querySelector(".burger-middle");
     var burgerBottom = document.querySelector(".burger-bottom");
     
     // toggle class
-    links.classList.toggle("links-reveal");
-    linksUl.classList.toggle("fade");
+    // links.classList.toggle("links-reveal");
+    // linksUl.classList.toggle("fade");
+    // burger.classList.toggle("content-justify-evenly");
+    // burger.classList.toggle("content-justify-center");
     burger.classList.toggle("animate-burger-border");
     burgerTop.classList.toggle("animate-top");
-    burgerMiddle.classList.toggle("animate-middle");
+    burgerMiddle.classList.toggle("hide");
     burgerBottom.classList.toggle("animate-bottom");
 
     // remove attributes
