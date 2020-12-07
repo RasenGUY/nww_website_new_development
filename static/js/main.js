479b1fcfc8768b5c9876536ca674c414176bf54b
rassenguy <ftrouw@protonmail.com>
2020-12-07 20:18:02 +0700

    nav links bottom animate properly

--- static/js/main.js
+++ static/js/main.js
@@ -3,9 +3,8 @@
 // menu-mobile-animation
 function animateMenu() {
     
-    // var links = document.querySelector(".wrapper-links-bottom");
-    // var linksUl = document.querySelector(".nav-links-bottom");
     // var navBtm = document.querySelector("nav.mobile-bottom");
+    var linksUl = document.querySelector("ul.nav-links-bottom");
     var burger = document.querySelector(".hamburger .burger");
     var burgerTop = document.querySelector(".burger-top");
     var burgerMiddle = document.querySelector(".burger-middle");
@@ -13,15 +12,16 @@ function animateMenu() {
     
     // toggle class
     // links.classList.toggle("links-reveal");
-    // linksUl.classList.toggle("fade");
-    // burger.classList.toggle("content-justify-evenly");
-    // burger.classList.toggle("content-justify-center");
+    // linksUl.style.display = "none";
+    // linksUl.classList.toggle("l-flex");
+    // linksUl.classList.toggle("col");
+    linksUl.classList.toggle("animate");
     burger.classList.toggle("animate-burger-border");
     burgerTop.classList.toggle("animate-top");
     burgerMiddle.classList.toggle("hide");
     burgerBottom.classList.toggle("animate-bottom");
 
     // remove attributes
-    navBtm.style.removeProperty("box-shadow");
+    // navBtm.style.removeProperty("box-shadow");
 
 }
