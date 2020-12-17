b6984bb54d5b1e102ff889ec84315ee8e79d382d
rassenguy <ftrouw@protonmail.com>
2020-12-17 14:31:14 +0700

    js code for creating responsiveness

--- static/js/main.js
+++ static/js/main.js
@@ -24,11 +24,13 @@ function animateMenu() {
     // navBtm.style.removeProperty("box-shadow");
 
     // toggle psuedoattributes
-    const cssRule = "nav#nav-mobile-links::before{transform: translateY(-5.5rem);}";
-    animateBefore(cssRule);
-}
+    var htmlEl = "nav#nav-mobile-links";
+    var cssRule = "transform: translateY(-5.5rem);"
+    var pseudo = "::before"
+    togglePseudo(htmlEl, pseudo, cssRule);
+};
 
-const animateBefore = (cssRule) => {
+var togglePseudo = (htmlEl, pseudo, cssRule) => { //function for animating pseudo-elements 
      
     // retrieve all childnodes 
     var children = document.querySelector("head").childNodes;
@@ -36,11 +38,12 @@ const animateBefore = (cssRule) => {
     // toggle the style  
     var ruleExists = false;
     var childIndex = null;
+    var pseudoEl = htmlEl + pseudo + "{"+ cssRule + "}"
 
     // remove existing style
     children.forEach((child, index) =>{
         console.log(child['innerHTML']);
-        if (child['innerHTML'] == cssRule){
+        if (child['innerHTML'] == pseudoEl){
             ruleExists = true; 
             childIndex = index;
         } 
@@ -57,9 +60,28 @@ const animateBefore = (cssRule) => {
 
         // add child
         var el = document.createElement("style");
-        el.innerHTML = cssRule;
+        el.innerHTML = pseudoEl;
+        console.log(el);
         document.querySelector("head").appendChild(el);
     }; 
 
     return 0;
-} 
+};
+
+// function for doing media queries on site 
+var Medqueries = (queryFunc, width, minMax)=>{
+    
+    // max or min
+    if minmax 
+    // initialize and listen to changes of page
+    let medQuery = window.mathcMedia(width);
+
+    queryFunc(query);
+    query.addlistener()
+};
+
+
+// responsive functions for sections of pages of site 
+const responsiveHome = () => {
+
+};  
