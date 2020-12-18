210c8d05b04e72be0083314b616a12b90083c0dd
rassenguy <ftrouw@protonmail.com>
2020-12-18 14:36:17 +0700

    failed attempt js reponsive s-one

---
+++ static/js/functions.js
@@ -0,0 +1,86 @@
+// all js functions used in the newworld site
+
+// function for toggling pseudo elements on and off
+export function togglePseudo(htmlEl, pseudo, cssRule){ //function for animating pseudo-elements 
+     
+    // retrieve all childnodes 
+    var children = document.querySelector("head").childNodes;
+
+    // toggle the style  
+    var ruleExists = false;
+    var childIndex = null;
+    var pseudoEl = htmlEl + pseudo + "{"+ cssRule + "}"
+
+    // remove existing style
+    children.forEach((child, index) =>{
+        console.log(child['innerHTML']);
+        if (child['innerHTML'] == pseudoEl){
+            ruleExists = true; 
+            childIndex = index;
+        } 
+    });
+
+    if (ruleExists == true){
+
+        // remove child
+        document.querySelector("head").removeChild(children[childIndex]);
+        console.log("removed child");
+
+    } 
+    else {
+
+        // add child
+        var el = document.createElement("style");
+        el.innerHTML = pseudoEl;
+        console.log(el);
+        document.querySelector("head").appendChild(el);
+    }; 
+
+    return 0;
+};
+
+// function for doing media queries on site 
+export function medQueries(width, minMax){
+    
+    let query = null; 
+    // max or min
+    if (minMax == "min"){ // if min then create min-width query
+        query = `(min-width: ${width})`;
+    }
+    else if (minMax == "max"){ // if min then create max-width query
+        query = `(max-width: ${width})`;
+    }
+
+    // initialize and listen to changes on page
+    let medQuery = window.mathcMedia(query);
+    return medQuery;
+};
+
+export function animateMenu(){
+
+    // variables to animate 
+    var navBtm = document.querySelector("nav#nav-mobile");
+    var linksUl = document.querySelector("ul.nav-links-bottom");
+    var burger = document.querySelector(".hamburger .burger");
+    var burgerTop = document.querySelector(".burger-top");
+    var burgerMiddle = document.querySelector(".burger-middle");
+    var burgerBottom = document.querySelector(".burger-bottom");
+    
+    // toggle class
+    linksUl.classList.toggle("animate");
+    burger.classList.toggle("animate-burger-border");
+    burgerTop.classList.toggle("animate-top");
+    burgerMiddle.classList.toggle("hide");
+    burgerBottom.classList.toggle("animate-bottom");
+    navBtm.classList.toggle("animate");
+    
+
+    // remove attributes
+    // navBtm.style.removeProperty("box-shadow");
+
+    // toggle psuedoattributes
+    var htmlEl = "nav#nav-mobile-links";
+    var cssRule = "transform: translateY(-5.5rem);"
+    var pseudo = "::before"
+    togglePseudo(htmlEl, pseudo, cssRule);
+};
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,17 @@
 // main js for the new new world website
+import {togglePseudo, medQueries} from "./functions.js"; 
 
 // menu-mobile-animation
-function animateMenu() {
+const animateMenu = document.querySelector("#nav-mobile .hamburger");
+animateMenu.addEventListener("click", () =>{
 
     // variables to animate 
-    var navBtm = document.querySelector("nav#nav-mobile");
-    var linksUl = document.querySelector("ul.nav-links-bottom");
-    var burger = document.querySelector(".hamburger .burger");
-    var burgerTop = document.querySelector(".burger-top");
-    var burgerMiddle = document.querySelector(".burger-middle");
-    var burgerBottom = document.querySelector(".burger-bottom");
+    const navBtm = document.querySelector("nav#nav-mobile");
+    const linksUl = document.querySelector("ul.nav-links-bottom");
+    const burger = document.querySelector(".hamburger .burger");
+    const burgerTop = document.querySelector(".burger-top");
+    const burgerMiddle = document.querySelector(".burger-middle");
+    const burgerBottom = document.querySelector(".burger-bottom");
     
     // toggle class
     linksUl.classList.toggle("animate");
@@ -24,64 +26,26 @@ function animateMenu() {
     // navBtm.style.removeProperty("box-shadow");
 
     // toggle psuedoattributes
-    var htmlEl = "nav#nav-mobile-links";
-    var cssRule = "transform: translateY(-5.5rem);"
-    var pseudo = "::before"
+    const htmlEl = "nav#nav-mobile-links";
+    const cssRule = "transform: translateY(-5.5rem);"
+    const pseudo = "::before"
     togglePseudo(htmlEl, pseudo, cssRule);
-};
-
-var togglePseudo = (htmlEl, pseudo, cssRule) => { //function for animating pseudo-elements 
-     
-    // retrieve all childnodes 
-    var children = document.querySelector("head").childNodes;
-
-    // toggle the style  
-    var ruleExists = false;
-    var childIndex = null;
-    var pseudoEl = htmlEl + pseudo + "{"+ cssRule + "}"
-
-    // remove existing style
-    children.forEach((child, index) =>{
-        console.log(child['innerHTML']);
-        if (child['innerHTML'] == pseudoEl){
-            ruleExists = true; 
-            childIndex = index;
-        } 
-    });
-
-    if (ruleExists == true){
-
-        // remove child
-        document.querySelector("head").removeChild(children[childIndex]);
-        console.log("removed child");
-
-    } 
-    else {
-
-        // add child
-        var el = document.createElement("style");
-        el.innerHTML = pseudoEl;
-        console.log(el);
-        document.querySelector("head").appendChild(el);
-    }; 
-
-    return 0;
-};
-
-// function for doing media queries on site 
-var Medqueries = (queryFunc, width, minMax)=>{
-    
-    // max or min
-    if minmax 
-    // initialize and listen to changes of page
-    let medQuery = window.mathcMedia(width);
-
-    queryFunc(query);
-    query.addlistener()
-};
+});
 
 
 // responsive functions for sections of pages of site 
-const responsiveHome = () => {
+// const responsiveHome = () => { 
 
-};  
+    // s-one (section one)
+    // select items
+    // const header = document.querySelector("#index-s-one > :nth-child(2)"); // textbox header
+
+    // change s-one items attributes give query
+//     if () {
+
+//     }
+    
+// };  
+
+// initialize queries
+// f.medQueries().addlistener();
