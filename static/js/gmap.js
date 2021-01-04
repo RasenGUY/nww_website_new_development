29ec129019c8b919021a8f376fc1c5e31d051a49
rassenguy <ftrouw@protonmail.com>
2021-01-04 13:04:35 +0700

    g-map feeds correctly into svg, form almost finsished

--- static/js/gmap.js
+++ static/js/gmap.js
@@ -1,19 +1,8 @@
-//javascript for maps
-
-let contentMap;
-window.addEventListener("load", function() {
-  svgcontent = document.querySelector(".svg-image.g-map");
-  contentMap = svgcontent; 
-});
-
+//javascript for map
 let map;
 function initMap() {
-  // map = new google.maps.Map(document.getElementById("g-map"), {
-    //   center: { lat: -34.397, lng: 150.644 },
-    //   zoom: 8,
-    // });
-    map = new google.maps.Map(contentMap.contentDocument.getElementById("g-map-test"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
+  map = new google.maps.Map(document.getElementById("g-map"), {
+      center: { lat: 20.44608, lng: 106.34772 },
+      zoom: 15,
     });
   }
