514a6c3ebea016a3162cf45425837e98d924883b
rassenguy <ftrouw@protonmail.com>
2021-01-11 14:34:57 +0700

    realized fucked things up with shapes :(, redo here we come :(

--- static/js/gmap.js
+++ static/js/gmap.js
@@ -3,6 +3,6 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("g-map"), {
       center: { lat: 20.44608, lng: 106.34772 },
-      zoom: 15,
+      zoom: 17,
     });
   }
